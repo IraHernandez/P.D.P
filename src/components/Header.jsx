a13ae17c140bab062fcd3f57cf0e9abcf09abb45
Gimena27 <66136801+Gimena27@users.noreply.github.com>
2021-09-15 11:55:56 -0500

    header responsive done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,26 +3,29 @@ import logo from "../img/logo.png";
 import menuGroupe from "../img/menuGroupe.svg";
 import Notification from "../img/Notification.svg";
 import "./Header.css";
-
+import logoMobile from "../img/logoMobile.svg"
 const Header = () => {
   return (
-    <div>
+    <>
       <header>
         <div className="header-logo ">
-          <img src={logo} alt="page logo" />
         </div>
-        <div className="header-menu ">
-          <img src={menuGroupe} alt="menu page" className="menu" />
-          <div className="barra"></div>
-          <img
-            src={Notification}
-            alt="notification logo"
-            className="notification"
-          />
-          <p>Cesar Avellaneda</p>
+        <div className="header-menu  ">
+          <div className="menu-and-bar">
+            <img src={menuGroupe} alt="menu page" className="menu" />
+            <div className="barra"></div>
+          </div>
+          <div className="notification-and-name ">
+            <img
+              src={Notification}
+              alt="notification logo"
+              className="notification"
+            />
+            <p>Cesar Avellaneda</p>
+          </div>
         </div>
       </header>
-    </div>
+    </>
   );
 };
 
